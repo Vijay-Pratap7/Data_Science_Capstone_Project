77b699b1b0e97712661e5b6a92fd44ef8358d244
VIJAY PRATAP <163440587+Vijay-Pratap7@users.noreply.github.com>
2024-04-17 17:31:06 +0530

    Update caps_app.py

--- caps_app.py
+++ caps_app.py
@@ -1,5 +1,4 @@
 import streamlit as st
-import pandas as pd
 import numpy as np
 import pickle
 
@@ -7,16 +6,13 @@ import pickle
 with open('rfmodel.pkl', 'rb') as file:
     model = pickle.load(file)
 
-# Load the encoder
-with open('encoder.pkl', 'rb') as file:
-    encoder = pickle.load(file)
-
 # Function to predict car price
 def predict_price(year, km_driven, fuel, seller_type, transmission, owner):
-    fuel_encoded = encoder.transform([fuel])[0]
-    seller_type_encoded = encoder.transform([seller_type])[0]
-    transmission_encoded = encoder.transform([transmission])[0]
-    owner_encoded = encoder.transform([owner])[0]
+    # Encode categorical variables
+    fuel_encoded = 1 if fuel == 'Petrol' else 0  # Assuming petrol is encoded as 1
+    seller_type_encoded = 1 if seller_type == 'Dealer' else 0  # Assuming dealer is encoded as 1
+    transmission_encoded = 1 if transmission == 'Automatic' else 0  # Assuming automatic is encoded as 1
+    owner_encoded = 0  # No need to encode for owner as it's already numeric
 
     car_details = np.array([year, km_driven, fuel_encoded, seller_type_encoded, transmission_encoded, owner_encoded]).reshape(1, -1)
     prediction = model.predict(car_details)
@@ -29,7 +25,7 @@ def main():
 
     year = st.slider('Year', 1990, 2023, 2010)
     km_driven = st.number_input('Kilometers Driven', min_value=0)
-    fuel = st.selectbox('Fuel Type', ['Diesel', 'Petrol', 'CNG', 'LPG'])
+    fuel = st.selectbox('Fuel Type', ['Petrol', 'Diesel', 'CNG', 'LPG'])
     seller_type = st.selectbox('Seller Type', ['Individual', 'Dealer', 'Trustmark Dealer'])
     transmission = st.selectbox('Transmission', ['Manual', 'Automatic'])
     owner = st.selectbox('Owner', ['First Owner', 'Second Owner', 'Third Owner', 'Fourth & Above Owner'])
